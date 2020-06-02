a5df3283ada1dd5d1d4d598e91b18f3f82fcf005
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-01 20:53:13 -0400

    Styled simulation page

--- app/src/Simulation.js
+++ app/src/Simulation.js
@@ -1,70 +1,55 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import "./Simulation.scss"
 
 const Simulation = () => {
 
   
   return (
-    <div>
-      <h1>Welcome to the simulation.</h1>
-      <h3>
+    <div className="sim-container">
+      <h1 className="sim-title">Welcome to the simulation.</h1>
+      {/* <p className="sim-description">Here you get to choose your choose your moves against your opponent.</p> */}
+
+      <div className="sim-intro-container">
+      <h3 className="sim-intro">
+
         You step onto the mat and shake your opponents hand. The match is about
         to begin. You have three options...
       </h3>
-      <ol>
-        <li>Take down your opponent </li>
-        <li>Pull Guard</li>
-        <li>Let them make the first move</li>
-      </ol>
-      <h3>What do you choose?</h3>
-      <ul>
+   
+      <h3 className="sim-choose">What do you choose?</h3>
+
+      <div className="sim-move-container">
+      <ul className="sim-move">
         <li>
-          {" "}
+          
           <Link to={`/takedowns`}>
-            <h2> Take down your opponent</h2>
+            <h2> I want to take down my opponent.</h2>
+            
           </Link>
         </li>
         <li>
-          {" "}
           <Link to={`/guardlist`}>
-            <h2> Pull guard on your opponent</h2>
+            <h2> I want to pull guard on my opponent.</h2>
           </Link>
         </li>
         <li>
           {" "}
           {/* <button onClick={randomNum}> */}
           <Link to={`/opponentmove`}>
-             <h2>Let your opponent make the first move</h2>
+             <h2>I want to let my opponent make the first move.</h2>
           </Link>
         
           {/* </button> */}
         </li>
-        {/* your opponent shoots in but you sprawl and land on top of them from here you spin and gain back control.
-              Your opponent tries to take you down but you manage to get him in your guard.
-              Math.random decide which one. Cmponent state changes based on which is picked. Onclick does htis. */}
-                 <div>
-      {/* {(() => {
-        if (num >= 2) {
-          return (
-            <div>        <h3>Your opponent tries to take you down but you manage to get them in your guard.Nice work!</h3>
-            <Link to={`/backcontrol`}><h3>Go to guard moves</h3></Link></div>
-          )
-        } else if (num <2) {
-          return (
-            <div> <h3>
-            Your opponent shoots and tries to take you down but you sprawl and land on top of them. From here you spin around and end up on the ground behind them Good work!.
-          </h3>
-          <Link to={`/mountlist`}><h3>Go to moves to use in back control</h3></Link></div>
-          )
-        }
-      })()} */}
-    </div>
+       
 
        
    
    
       </ul>
+      </div>
+      </div>
     </div>
   );
 };
